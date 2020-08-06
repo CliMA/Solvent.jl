bcbbfba985b1513657783f00372425d3a32e5952
jbmackay <jbmackay@caltech.edu>
2020-08-05 17:54:11 -0700

    Add preliminary benchmarking for GMRES packages.

--- test/test_gmres.jl
+++ test/test_gmres.jl
@@ -5,77 +5,84 @@ using LinearAlgebra
 using SparseArrays
 using Random
 using Printf
+using IterativeSolvers
+using KrylovKit
+using Krylov
 
 # this test setup is partly based on IterativeSolvers.jl, see e.g
 # https://github.com/JuliaMath/IterativeSolvers.jl/blob/master/test/cg.jl
-@testset "Solvent small full system" begin
-    n = 10
+# @testset "Solvent small full system" begin
+#     n = 10
 
-    for T in [Float32, Float64]
-        Random.seed!(44)
+#     for T in [Float32, Float64]
+#         Random.seed!(44)
 
-        A = @MMatrix rand(T, n, n)
-        A = A' * A + I
-        b = @MVector rand(T, n)
+#         A = @MMatrix rand(T, n, n)
+#         A = A' * A + I
+#         b = @MVector rand(T, n)
 
-        mulbyA!(y, x) = (y .= A * x)
+#         mulbyA!(y, x) = (y .= A * x)
 
-        x = @MVector rand(T, n)
+#         x = @MVector rand(T, n)
 
-        tol = sqrt(eps(T))
-        solver_type = GeneralizedMinimalResidualMethod(M = n, K = 1)
-        preconditioner = Identity(pc_side=PCleft())
-        linearsolver = LinearSolver(
-            mulbyA!,
-            solver_type,
-            preconditioner,
-            x;
-            rtol = tol,
-            atol = tol,
-        )
+#         tol = sqrt(eps(T))
+#         solver_type = GeneralizedMinimalResidualMethod(M = n, K = 1)
+#         preconditioner = Solvent.Identity(pc_side=PCleft())
+#         linearsolver = LinearSolver(
+#             mulbyA!,
+#             solver_type,
+#             preconditioner,
+#             x;
+#             rtol = tol,
+#             atol = tol,
+#         )
 
-        x0 = copy(x)
-        linearsolve!(linearsolver, x, b)
-        @test norm(A * x - b) / norm(A * x0 - b) <= tol
+#         x0 = copy(x)
+#         linearsolve!(linearsolver, x, b)
+#         @test norm(A * x - b) / norm(A * x0 - b) <= tol
 
-        # test for convergence in 0 iterations by
-        # initializing with the exact solution
-        x = A \ b
-        iters = linearsolve!(linearsolver, x, b)
-        @test iters == 0
-        @test norm(A * x - b) <= 100 * eps(T)
+#         # test for convergence in 0 iterations by
+#         # initializing with the exact solution
+#         x = A \ b
+#         iters = linearsolve!(linearsolver, x, b)
+#         @test iters == 0
+#         @test norm(A * x - b) <= 100 * eps(T)
 
-        newtol = 1000tol
-        settolerance!(linearsolver, newtol)
-        settolerance!(linearsolver, newtol; relative = true)
+#         newtol = 1000tol
+#         settolerance!(linearsolver, newtol)
+#         settolerance!(linearsolver, newtol; relative = true)
 
-        x = @MVector rand(T, n)
-        x0 = copy(x)
-        linearsolve!(linearsolver, x, b)
+#         x = @MVector rand(T, n)
+#         x0 = copy(x)
+#         linearsolve!(linearsolver, x, b)
 
-        @test norm(A * x - b) / norm(A * x0 - b) <= newtol
-        @test norm(A * x - b) / norm(A * x0 - b) >= tol
+#         @test norm(A * x - b) / norm(A * x0 - b) <= newtol
+#         @test norm(A * x - b) / norm(A * x0 - b) >= tol
 
-    end
-end
+#     end
+# end
 
 @testset "Solvent large sparse system" begin
     n = 10000
+    M = 20
+    K = 10
 
     for T in [Float32, Float64]
         Random.seed!(44)
 
-        α = 1e-2
+        α = 1f-2
         A = I + α * sprandn(T, n, n, 0.05)
         b = rand(T, n)
+        println(eltype(α), α)
+
 
         mulbyA!(y, x) = (y .= A * x)
 
         x = rand(T, n)
 
         tol = sqrt(eps(T))
-        solver_type = GeneralizedMinimalResidualMethod(M = 20, K = 10)
-        preconditioner = Identity(pc_side=PCright())
+        solver_type = GeneralizedMinimalResidualMethod(M = M, K = K)
+        preconditioner = Solvent.Identity(pc_side=PCright())
         linearsolver = LinearSolver(
             mulbyA!,
             solver_type,
@@ -86,7 +93,18 @@ end
         )
 
         x0 = copy(x)
-        linearsolve!(linearsolver, x, b)
+        x1 = copy(x)
+        @timev linearsolve!(linearsolver, x, b)
+        @timev gmres!(x1, A, b, tol = tol, restart = M, maxiter = K)
+        # specify gmres by isposdef == false, cg if isposdef == true
+        @timev begin
+            x2, info = linsolve(A, b;
+                krylovdim = M, maxiter = K,
+                rtol = tol, atol = tol,
+                isposdef = false)
+        end
+        #@timev x3 = dqgmres(A, b; atol = tol, rtol = tol, memory = M, itmax = K)
+        println("x:", norm(x), "x3:", norm(x3), "x-x3:", norm(x - x3))
         @test norm(A * x - b) / norm(A * x0 - b) <= tol
 
         # test for convergence in 0 iterations by
@@ -105,6 +123,5 @@ end
         linearsolve!(linearsolver, x, b)
 
         @test norm(A * x - b) / norm(A * x0 - b) <= newtol
-
     end
 end
